60e5fb4033346d3bcfee0ceabb3d9886e61ee6f9
Felipe Roscoche <felipe.roscoche@lexisnexisrisk.com>
2018-01-11 10:46:08 -0200

    corrected names

--- install_scripts/kibana_instance.sh
+++ install_scripts/kibana_instance.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
+echo "Making default ELK directory, downloading, extracting and renaming the folders"
 mkdir /elk
 cd /elk
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.1.1.zip
+wget https://artifacts.elastic.co/downloads/kibana/kibana-6.1.1-linux-x86_64.tar.gz
 wget https://artifacts.elastic.co/downloads/logstash/logstash-6.1.1.zip
-unzip elasticsearch-6.1.1.zip
+tar -xvzf kibana-6.1.1-linux-x86_64.tar.gz
 unzip logstash-6.1.1.zip
-mv elasticsearch-6.1.1 elasticsearch
+mv kibana-6.1.1-linux-x86_64 kibana
 mv logstash-6.1.1 logstash
 
-rm *.zip
+echo "Cleaning"
+rm *.zip *.gz
+
+echo "Downloading configuration files..."
+wget https://raw.githubusercontent.com/roscoche/ELK_tutorial/master/install_scripts/config_files/kibana.yml /elk/kibana/config/
+wget https://raw.githubusercontent.com/roscoche/ELK_tutorial/master/install_scripts/config_files/logstash.yml /elk/logstash/config/
+wget https://raw.githubusercontent.com/roscoche/ELK_tutorial/master/install_scripts/config_files/logstash.yml /elk/logstash/config/
+
+echo "Downloading service script..."
+sudo wget https://raw.githubusercontent.com/roscoche/ELK_tutorial/master/install_scripts/services/kibana /etc/init.d/
+sudo wget https://raw.githubusercontent.com/roscoche/ELK_tutorial/master/install_scripts/services/logstash /etc/init.d/
+sudo initctl reload-configuration
+sudo service logstash start
+sudo service kibana start
+
+logout
+
+
+
+
--- install_scripts/logstash_instance.sh
+++ install_scripts/logstash_instance.sh
@@ -2,11 +2,14 @@
 
 mkdir /elk
 cd /elk
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.1.1.zip
+
 wget https://artifacts.elastic.co/downloads/logstash/logstash-6.1.1.zip
-unzip elasticsearch-6.1.1.zip
 unzip logstash-6.1.1.zip
-mv elasticsearch-6.1.1 elasticsearch
 mv logstash-6.1.1 logstash
 
 rm *.zip
+cd config
+rm logstash.yml
+
+wget 
+echo 
